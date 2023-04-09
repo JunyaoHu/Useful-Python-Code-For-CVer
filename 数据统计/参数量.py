83db74deacc94e6050938b1a14a837d6d5509988
hujunyao <1078622540@qq.com>
2023-04-09 23:12:46 +0800

    update

---
+++ 数据统计/参数量.py
@@ -0,0 +1,41 @@
+"""
+# 用途：参数量计算
+"""
+
+import torch.nn as nn
+
+def count_training_parameters(model):
+    res = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f"count_training_parameters: {res}")
+
+def count_parameters(model):
+    res = sum(p.numel() for p in model.parameters())
+    print(f"count_parameters: {res}")
+
+class Model(nn.Module):
+    def __init__(self, num_inputs, num_hiddens, num_outputs):
+        super().__init__()
+        self.hidden = nn.Linear(num_inputs, num_hiddens)
+        self.act = nn.ReLU()
+        self.output = nn.Linear(num_hiddens, num_outputs)
+
+    def forward(self, x):
+        return self.output(self.act(self.hidden(x)))
+
+if __name__ == '__main__':
+    num_inputs = 100
+    num_hiddens = 200
+    num_outputs = 2
+    num_ins = 2
+
+    net = Model(num_hiddens=num_hiddens, num_outputs=num_outputs, num_inputs=num_inputs)
+
+    count_training_parameters(net)
+    count_parameters(net)
+
+    
+"""
+# 样例输出
+count_training_parameters: 20602
+count_parameters: 20602
+"""
