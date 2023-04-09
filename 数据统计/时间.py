401462565f93dd4236a37ce7ae848e50d376cac0
hujunyao <1078622540@qq.com>
2023-04-09 21:14:16 +0800

    init my repo

---
+++ 数据统计/时间.py
@@ -0,0 +1,22 @@
+"""
+# 用途：时间统计
+"""
+
+import time
+
+start = time.time()
+
+print(f"time: {time.time()-start}")
+
+for i in range(int(1e6)):
+    continue
+
+print(f"time: {time.time()-start}")
+
+"""
+# 输入示例
+无
+# 输出示例
+time: 0.0
+time: 0.014991998672485352
+"""
